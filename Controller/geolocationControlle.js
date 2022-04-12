cb78842e19637a097da7691f17e15f79d47d104f
yagormorares <yago.dmoraes@gmail.com>
2022-04-12 11:03:23 -0300

    Complementação da Model

---
+++ Controller/geolocationControlle.js
@@ -0,0 +1,2 @@
+import GoogleAPI from "../Service/googleAPI.js";
+import geolocationModel from "../Model/geolocationModel.js";
--- Model/geolocationModel.js
+++ Model/geolocationModel.js
@@ -1,8 +1,57 @@
 class geolocationModel{
-    euclidianDistance(a,b){
-        return a
-        .map((x, i) => Math.abs( x - b[i] ) ** 2)
-        .reduce((sum, now) => sum + now)
-        ** (1/2)
+    euclidianDistance(x1,x2,y1,y2){
+        let x =Math.abs(x2-x1)
+        let y = Math.abs(y2-y1)
+        return Math.sqrt((x**2) + (y**2))
     }
+
+    multipleDistances(array){
+        let distanceArray = []
+        for (let i = 0; i< distanceArray.length; i++){
+            for(let j=0; j<distanceArray.length; j++){
+                let calulatedValue = this.euclidianDistance(
+                    array[i].coord.lat,
+                    array[j].coord.lat,
+                    array[i].coord.lng,
+                    array[j].coord.lng
+                )
+                const wayTo = {
+                    start:`${array[i].coordValue}`,
+                    end: `${array[j].coordValue}`
+                }
+
+                const distance = {
+                    way: wayTo,
+                    dist: calulatedValue 
+                }
+                distanceArray.push(distance)
+            }
+        }
+        return distanceArray
+    }
+
+    orderDistance(array){
+        let populated = this.multipleDistances(array).sort((a,b)=>{
+            if(a.dist < b.dist){return -1}
+            else if(a.dist > b.dist){return 1}
+            else{ return 0}
+
+        })
+        return populated
+    }
+
+    itIsLocal(array){
+        let list = this.orderDistance(array)
+        let empty = 0
+        list = list.filter(item => item.dist!=empty)
+        let last = list.length -1
+        return{
+            neaby:list[0],
+            farAway:list[last]
+        }
+    }
+
+
 }
+
+export default geolocationModel
