caf120f4d24c0ea87fa4c4f24328e44b28b43ccf
yagormorares <yago.dmoraes@gmail.com>
2022-04-12 11:13:20 -0300

    Adição da controller

--- Controller/geolocationControlle.js
+++ Controller/geolocationControlle.js
@@ -1,2 +1,20 @@
 import GoogleAPI from "../Service/googleAPI.js";
 import geolocationModel from "../Model/geolocationModel.js";
+
+const geolocationController = (app) =>{
+    const api = new GoogleAPI()
+    const model = new geolocationModel()
+
+    app.get('/', (req,res)=>{
+        const queryValues = Object.values(req.query)
+        api.coordinatesGroup(queryValues)
+        .then((groupBy)=>{
+            res.status(200).json(model.itIsLocal(groupBy))
+        })
+        .catch((e)=>{
+            res.status(404).json(e.message)
+        })
+    })
+}
+
+export default geolocationController
